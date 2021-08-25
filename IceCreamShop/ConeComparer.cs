148d7faa408decc6f25dc6133a673444862dce7c
Donna Seidel <dseidel01@gmail.com>
2021-08-25 18:56:41 -0500

    flavorcomparer and conecomparer implemented

---
+++ IceCreamShop/ConeComparer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShop
+{
+    class ConeComparer :  IComparer<Cone>
+    {
+        public int Compare(Cone x, Cone y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,27 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
+            Console.WriteLine("\nflavors b4 sort\n");
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+            /*            FlavorComparer comparer = new FlavorComparer();
+                        availableFlavors.Sort(comparer);*/
+            //OR combine
+            availableFlavors.Sort(new FlavorComparer());
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            Console.WriteLine("\nflavors after sort\n");
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
         }
     }
 }
